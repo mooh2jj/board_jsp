8afd6c4de108b5f096092cccbec523868b6750cb
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-22 17:57:54 +0900

    close() ConnectionUtil 클래스에 옮김

--- src/connection/DBConnectionUtil.java
+++ src/connection/DBConnectionUtil.java
@@ -1,7 +1,6 @@
 package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 import static connection.ConnectionConst.*;
 
@@ -15,6 +14,39 @@ public class DBConnectionUtil {
             return connection;
         } catch (Exception e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * 커넥션 객체 반환하기 위한 close 메서드
+     * @param con
+     * @param pstmt
+     * @param rs
+     */
+    // TODO: Utils에 투입
+    public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();   // SQLException 터져도 여기서 나올 수 있어
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
